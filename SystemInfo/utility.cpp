3ca7b0a8a46ab8876820dc3ddd7ab942fe37555c
CherryPill <toshioshitsu@gmail.com>
2017-06-26 14:34:05 +0300

    Various fixes

--- SystemInfo/utility.cpp
+++ SystemInfo/utility.cpp
@@ -159,7 +159,7 @@ UINT32 adjustItemHeight(HWND windowHandle, UINT32 ITEM_ID, UINT32 innerItemsCoun
 	UINT32 adjustedItemHeight;
 	UINT32 adjustedYAxisOffset;
 	adjustedItemHeight = innerItemsCount * 15;
-	adjustedYAxisOffset = itemHandleDimensions.top + adjustedItemHeight;
+	adjustedYAxisOffset = itemHandleDimensions.top + adjustedItemHeight + 5;
 	return adjustedYAxisOffset;
 }
 UINT32 isAdjustRequired(UINT32 ITEM_ID, SystemInfo *info) {
